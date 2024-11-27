0940d304d1993686b97aace027065d9483189127
greensnapback0229 <smdmim@gmail.com>
2024-11-27 11:00:15 +0900

    solve(baekjoon_9095): 1, 2, 3 더하기

---
+++ 1, 2, 3 더하기/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static Integer[] dp = new Integer[11];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+        int N = Integer.parseInt(br.readLine());
+        int T[] = new int[N];
+
+
+        for(int i=0;i<N;i++){
+            T[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int n=0;n<N;n++){
+            for(int i=4;i<=T[n];i++){
+                if(dp[i] == null){
+                    dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
+                }
+            }
+            bw.write(String.valueOf(dp[T[n]] + "\n"));
+        }
+        
+        bw.close();
+        
+    }
+}
